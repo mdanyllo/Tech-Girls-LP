68dc75b0222efb3e93ac0315288547ddef701b20
mdanyllo <markusdanyllo07@gmail.com>
2025-09-18 14:53:53 -0300

    somente mobile

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -13,9 +13,9 @@ export default function Contact () {
                 <Image alt="Email" width={80} height={100} src="/instagram.png"></Image>
             </Link>
             </div>
-            <h1 className="md:text-3xl text-2xl text-center mt-10" >Acesse nosso Instagram</h1>
-            <p className="md:text-xl text-base text-center mb-6">Aponte a camêra do seu celular</p>
-            <Image className=""  alt="QR Code Instagram" width={400} height={150} src="/qrcode.png"></Image>
+            <h1 className="md:text-3xl text-2xl text-center mt-10 hidden md:block" >Acesse nosso Instagram</h1>
+            <p className="md:text-xl text-base text-center mb-6 hidden md:block">Aponte a camêra do seu celular</p>
+            <Image className="hidden md:block"  alt="QR Code Instagram" width={400} height={150} src="/qrcode.png"></Image>
         </div>
     )
 }
